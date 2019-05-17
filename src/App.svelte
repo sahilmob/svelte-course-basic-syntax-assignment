8a88db9514acfa699b7da81f5d87eb2799e1065f
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-17 18:44:58 +0300

    add input and bind to courseGoal

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,8 @@
+<script>
+  let courseGoal = "";
+</script>
+
+<input type="text" bind:value={courseGoal} />
 <h1>Assignment</h1>
 
 <p>Solve these tasks.</p>
@@ -6,9 +11,11 @@
   <li>Add an input field that allows users to enter a course goal.</li>
   <li>Output the user input in a h1 tag.</li>
   <li>
-    Color the output red (e.g. by adding a class) if it contains at least one exclamation mark.
+    Color the output red (e.g. by adding a class) if it contains at least one
+    exclamation mark.
   </li>
   <li>
-    Put the h1 tag + output into a separate component to which you pass the user input
+    Put the h1 tag + output into a separate component to which you pass the user
+    input
   </li>
 </ol>
